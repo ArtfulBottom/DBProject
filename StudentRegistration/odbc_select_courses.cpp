5c245577fc9c6153505470cea2e533531ff5225f
pastmidnight14 <pwm001@uark.edu>
2017-04-23 18:28:06 -0500

    test select deptCode

--- StudentRegistration/odbc_select_courses.cpp
+++ StudentRegistration/odbc_select_courses.cpp
@@ -11,6 +11,7 @@ int main(int argc, char *argv[]) {
 		q = "SELECT DISTINCT DeptCode FROM Course;";
 	} else {	//Return courses from specified department
 		string dept = argv[1];
+		cout << "dept=" << dept << endl;
 		q = "SELECT * FROM Course WHERE DeptCode = " + dept + ";";
 	}
 
