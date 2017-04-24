5133eceb0da4622541b8501fc23bc96cfd9183bc
Luke Snyder <ls008@uark.edu>
2017-04-23 22:29:08 -0500

    Removed extra table.

--- StudentRegistration/odbc_select_studentCourses.php
+++ StudentRegistration/odbc_select_studentCourses.php
@@ -18,18 +18,6 @@ tr:nth-child(even) {
 <body>
 <h1>Enter a Student ID to obtain their enrolled course information:</h1>
 
-<?php
-	$config = include('config.php');
-	$command = $config['path'] . 'odbc_select_studentCourses.exe';
-		
-	// remove dangerous characters from command to protect web server
-	$command = escapeshellcmd($command);
-
-	// run odbc_select_studentCourses.exe
-	system($command);         
-?>
-
-<br>
 <form action="odbc_select_studentCourses.php" method="post">
     Enter a Student ID: <input type="text" name="studentId" required><br><br>
     <input name="Submit" type="submit">
@@ -40,6 +28,7 @@ tr:nth-child(even) {
 
 <?php
 	if (isset($_POST['studentId'])) {
+		$config = include('config.php');
     	$command = $config['path'] . 'odbc_select_studentCourses.exe ' . $_POST['studentId']; 
 
 		// remove dangerous characters from command to protect web server
